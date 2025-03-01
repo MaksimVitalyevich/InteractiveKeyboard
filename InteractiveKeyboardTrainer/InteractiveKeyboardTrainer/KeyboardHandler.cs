be9fc9a1fe9f302603944812442fbb7468c5fb63
MaksimVitalyevich <zennat.3195@gmail.com>
2025-03-01 10:47:33 +0700

    debug version

---
+++ InteractiveKeyboardTrainer/InteractiveKeyboardTrainer/KeyboardHandler.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace InteractiveKeyboardTrainer
+{
+    // Класс для перечисления обработок клавиш при нажатии KeyDown и отпускании KeyUp
+    public class KeyboardHandler
+    {
+        private readonly Dictionary<Key, Action> keyDownActions;
+        private readonly Dictionary<Key, Action> keyUpActions;
+        bool ShiftPressed => Keyboard.IsKeyDown(Key.LeftShift) || Keyboard.IsKeyDown(Key.RightShift);
+        bool CapsLockPressed => Keyboard.GetKeyStates(Key.CapsLock) == KeyStates.Toggled;
+        public KeyboardHandler(MainWindow mainWindow)
+        {
+            keyDownActions = new Dictionary<Key, Action>
+            {
+                {Key.OemTilde, () => HighlightButton(mainWindow.btn_Tilda, ShiftPressed || CapsLockPressed ? "~" : "`") },
+                {Key.D0, () => HighlightButton(mainWindow.btn_Zero, ShiftPressed || CapsLockPressed ? ")" : "0") },
+                {Key.D1, () => HighlightButton(mainWindow.btn_One, ShiftPressed || CapsLockPressed ? "!" : "1") },
+                {Key.D2, () => HighlightButton(mainWindow.btn_Two, ShiftPressed || CapsLockPressed ? "@" : "2") },
+                {Key.D3, () => HighlightButton(mainWindow.btn_Three, ShiftPressed || CapsLockPressed ? "#" : "3") },
+                {Key.D4, () => HighlightButton(mainWindow.btn_Four, ShiftPressed || CapsLockPressed ? "$" : "4") },
+                {Key.D5, () => HighlightButton(mainWindow.btn_Five, ShiftPressed || CapsLockPressed ? "%" : "5") },
+                {Key.D6, () => HighlightButton(mainWindow.btn_Six, ShiftPressed || CapsLockPressed ? "^" : "6") },
+                {Key.D7, () => HighlightButton(mainWindow.btn_Seven, ShiftPressed || CapsLockPressed ? "&" : "7") },
+                {Key.D8, () => HighlightButton(mainWindow.btn_Eight, ShiftPressed || CapsLockPressed ? "*" : "8") },
+                {Key.D9, () => HighlightButton(mainWindow.btn_Nine, ShiftPressed || CapsLockPressed ? "(" : "9") },
+                {Key.A, () => HighlightButton(mainWindow.btn_A, ShiftPressed || CapsLockPressed ? "A" : "a") },
+                {Key.B, () => HighlightButton(mainWindow.btn_B, ShiftPressed || CapsLockPressed ? "B" : "b") },
+                {Key.C, () => HighlightButton(mainWindow.btn_C, ShiftPressed || CapsLockPressed ? "C" : "c") },
+                {Key.D, () => HighlightButton(mainWindow.btn_D, ShiftPressed || CapsLockPressed ? "D" : "d") },
+                {Key.E, () => HighlightButton(mainWindow.btn_E, ShiftPressed || CapsLockPressed ? "E" : "e") },
+                {Key.F, () => HighlightButton(mainWindow.btn_F, ShiftPressed || CapsLockPressed ? "F" : "f") },
+                {Key.G, () => HighlightButton(mainWindow.btn_G, ShiftPressed || CapsLockPressed ? "G" : "g") },
+                {Key.H, () => HighlightButton(mainWindow.btn_H, ShiftPressed || CapsLockPressed ? "H" : "h") },
+                {Key.I, () => HighlightButton(mainWindow.btn_I, ShiftPressed || CapsLockPressed ? "I" : "i") },
+                {Key.J, () => HighlightButton(mainWindow.btn_J, ShiftPressed || CapsLockPressed ? "J" : "j") },
+                {Key.K, () => HighlightButton(mainWindow.btn_K, ShiftPressed || CapsLockPressed ? "K" : "k") },
+                {Key.L, () => HighlightButton(mainWindow.btn_L, ShiftPressed || CapsLockPressed ? "L" : "l") },
+                {Key.M, () => HighlightButton(mainWindow.btn_M, ShiftPressed || CapsLockPressed ? "M" : "m") },
+                {Key.N, () => HighlightButton(mainWindow.btn_N, ShiftPressed || CapsLockPressed ? "N" : "n") },
+                {Key.O, () => HighlightButton(mainWindow.btn_O, ShiftPressed || CapsLockPressed ? "O" : "o") },
+                {Key.P, () => HighlightButton(mainWindow.btn_P, ShiftPressed || CapsLockPressed ? "P" : "p") },
+                {Key.Q, () => HighlightButton(mainWindow.btn_Q, ShiftPressed || CapsLockPressed ? "Q" : "q") },
+                {Key.R, () => HighlightButton(mainWindow.btn_R, ShiftPressed || CapsLockPressed ? "R" : "r") },
+                {Key.S, () => HighlightButton(mainWindow.btn_S, ShiftPressed || CapsLockPressed ? "S" : "s") },
+                {Key.T, () => HighlightButton(mainWindow.btn_T, ShiftPressed || CapsLockPressed ? "T" : "t") },
+                {Key.U, () => HighlightButton(mainWindow.btn_U, ShiftPressed || CapsLockPressed ? "U" : "u") },
+                {Key.V, () => HighlightButton(mainWindow.btn_V, ShiftPressed || CapsLockPressed ? "V" : "v") },
+                {Key.W, () => HighlightButton(mainWindow.btn_W, ShiftPressed || CapsLockPressed ? "W" : "w") },
+                {Key.X, () => HighlightButton(mainWindow.btn_X, ShiftPressed || CapsLockPressed ? "X" : "x") },
+                {Key.Y, () => HighlightButton(mainWindow.btn_Y, ShiftPressed || CapsLockPressed ? "Y" : "y") },
+                {Key.Z, () => HighlightButton(mainWindow.btn_Z, ShiftPressed || CapsLockPressed ? "Z" : "z") },
+                {Key.Oem1, () => HighlightButton(mainWindow.btn_SemiColon, ShiftPressed || CapsLockPressed ? ":" : ";") },
+                {Key.Oem2, () => HighlightButton(mainWindow.btn_Question, ShiftPressed || CapsLockPressed ? "?" : "/") },
+                {Key.Oem4, () => HighlightButton(mainWindow.btn_SBLeft, ShiftPressed || CapsLockPressed ? "{" : "[") },
+                {Key.Oem5, () => HighlightButton(mainWindow.btn_Pipe, ShiftPressed || CapsLockPressed ? "|" : "\\") },
+                {Key.Oem6, () => HighlightButton(mainWindow.btn_SBRight, ShiftPressed || CapsLockPressed ? "}" : "]") },
+                {Key.Oem7, () => HighlightButton(mainWindow.btn_Quote, ShiftPressed || CapsLockPressed ? "\"" : "'") },
+                {Key.OemComma, () => HighlightButton(mainWindow.btn_Comma, ShiftPressed || CapsLockPressed ? "<" : ",") },
+                {Key.OemPeriod, () => HighlightButton(mainWindow.btn_Dot, ShiftPressed || CapsLockPressed ? ">" : ".") },
+                {Key.OemPlus, () => HighlightButton(mainWindow.btn_Equal, ShiftPressed || CapsLockPressed ? "+" : "=") },
+                {Key.OemMinus, () => HighlightButton(mainWindow.btn_Minus, ShiftPressed || CapsLockPressed ? "_" : "-") },
+                {Key.Space, () => HighlightButton(mainWindow.btn_Space, "Space") },
+                {Key.Back, () => HighlightButton(mainWindow.btn_BS, "BackSpace") },
+                {Key.Tab, () => HighlightButton(mainWindow.btn_Tab, "Tab") },
+                {Key.LeftAlt, () => HighlightButton(mainWindow.btn_LeftAlt, "Alt") },
+                {Key.LeftCtrl, () => HighlightButton(mainWindow.btn_LeftCtrl, "Ctrl") },
+                {Key.LeftShift, () => HighlightButton(mainWindow.btn_LeftShift, "Shift") },
+                {Key.RightAlt, () => HighlightButton(mainWindow.btn_RightAlt, "Alt") },
+                {Key.RightCtrl, () => HighlightButton(mainWindow.btn_RightCtrl, "Ctrl") },
+                {Key.RightShift, () => HighlightButton(mainWindow.btn_RightShift, "Shift") },
+                {Key.Enter, () => HighlightButton(mainWindow.btn_Enter, "Enter") },
+                {Key.LWin, () =>HighlightButton(mainWindow.btn_LeftWin, "Win") },
+                {Key.RWin, () => HighlightButton(mainWindow.btn_RightWin, "Win") }
+            };
+            keyUpActions = new Dictionary<Key, Action>
+            {
+                {Key.OemTilde, () => ResetHighlight(mainWindow.btn_Tilda, "LightCoral") },
+                {Key.D0, () => ResetHighlight(mainWindow.btn_Zero, "Yellow") },
+                {Key.D1, () => ResetHighlight(mainWindow.btn_One, "LightCoral") },
+                {Key.D2, () => ResetHighlight(mainWindow.btn_Two, "LightCoral") },
+                {Key.D3, () => ResetHighlight(mainWindow.btn_Three, "Yellow") },
+                {Key.D4, () => ResetHighlight(mainWindow.btn_Four, "LightGreen") },
+                {Key.D5, () => ResetHighlight(mainWindow.btn_Five, "LightBlue") },
+                {Key.D6, () => ResetHighlight(mainWindow.btn_Six, "LightBlue") },
+                {Key.D7, () => ResetHighlight(mainWindow.btn_Seven, "HotPink") },
+                {Key.D8, () => ResetHighlight(mainWindow.btn_Eight, "HotPink") },
+                {Key.D9, () => ResetHighlight(mainWindow.btn_Nine, "LightCoral") },
+                {Key.A, () => ResetHighlight(mainWindow.btn_A, "LightCoral") },
+                {Key.B, () => ResetHighlight(mainWindow.btn_B, "LightBlue") },
+                {Key.C, () => ResetHighlight(mainWindow.btn_C, "LightGreen") },
+                {Key.D, () => ResetHighlight(mainWindow.btn_D, "LightBlue") },
+                {Key.E, () => ResetHighlight(mainWindow.btn_E, "LightGreen") },
+                {Key.F, () => ResetHighlight(mainWindow.btn_F, "LightBlue") },
+                {Key.G, () => ResetHighlight(mainWindow.btn_G, "LightBlue") },
+                {Key.H, () => ResetHighlight(mainWindow.btn_H, "HotPink") },
+                {Key.I, () => ResetHighlight(mainWindow.btn_I, "LightCoral") },
+                {Key.J, () => ResetHighlight(mainWindow.btn_J, "HotPink") },
+                {Key.K, () => ResetHighlight(mainWindow.btn_K, "LightCoral") },
+                {Key.L, () => ResetHighlight(mainWindow.btn_L, "Yellow") },
+                {Key.M, () => ResetHighlight(mainWindow.btn_M, "HotPink") },
+                {Key.N, () => ResetHighlight(mainWindow.btn_N, "HotPink") },
+                {Key.O, () => ResetHighlight(mainWindow.btn_O, "Yellow") },
+                {Key.P, () => ResetHighlight(mainWindow.btn_P, "LightGreen") },
+                {Key.Q, () => ResetHighlight(mainWindow.btn_Q, "LightCoral") },
+                {Key.R, () => ResetHighlight(mainWindow.btn_R, "LightBlue") },
+                {Key.S, () => ResetHighlight(mainWindow.btn_S, "Yellow") },
+                {Key.T, () => ResetHighlight(mainWindow.btn_T, "LightBlue") },
+                {Key.U, () => ResetHighlight(mainWindow.btn_U, "HotPink") },
+                {Key.V, () => ResetHighlight(mainWindow.btn_V, "LightBlue") },
+                {Key.W, () => ResetHighlight(mainWindow.btn_W, "Yellow") },
+                {Key.X, () => ResetHighlight(mainWindow.btn_X, "Yellow") },
+                {Key.Y, () => ResetHighlight(mainWindow.btn_Y, "HotPink") },
+                {Key.Z, () => ResetHighlight(mainWindow.btn_Z, "LightCoral") },
+                {Key.Oem1, () => ResetHighlight(mainWindow.btn_SemiColon, "LightGreen") },
+                {Key.Oem2, () => ResetHighlight(mainWindow.btn_Question, "LightGreen") },
+                {Key.Oem4, () => ResetHighlight(mainWindow.btn_SBLeft, "LightGreen") },
+                {Key.Oem5, () => ResetHighlight(mainWindow.btn_Pipe, "LightGreen") },
+                {Key.Oem6, () => ResetHighlight(mainWindow.btn_SBRight, "LightGreen") },
+                {Key.Oem7, () => ResetHighlight(mainWindow.btn_Quote, "LightGreen") },
+                {Key.OemComma, () => ResetHighlight(mainWindow.btn_Comma, "LightCoral") },
+                {Key.OemPeriod, () => ResetHighlight(mainWindow.btn_Dot, "Yellow") },
+                {Key.OemPlus, () => ResetHighlight(mainWindow.btn_Equal, "LightGreen") },
+                {Key.OemMinus, () => ResetHighlight(mainWindow.btn_Minus, "LightGreen") },
+                {Key.Space, () => ResetHighlight(mainWindow.btn_Space, "Orange") },
+                {Key.Back, () => ResetHighlight(mainWindow.btn_BS, "White") },
+                {Key.Tab, () => ResetHighlight(mainWindow.btn_Tab, "White") },
+                {Key.LeftAlt, () => ResetHighlight(mainWindow.btn_LeftAlt, "White") },
+                {Key.LeftCtrl, () => ResetHighlight(mainWindow.btn_LeftCtrl, "White") },
+                {Key.LeftShift, () => ResetHighlight(mainWindow.btn_LeftShift, "White") },
+                {Key.RightAlt, () => ResetHighlight(mainWindow.btn_RightAlt, "White") },
+                {Key.RightCtrl, () => ResetHighlight(mainWindow.btn_RightCtrl, "White") },
+                {Key.RightShift, () => ResetHighlight(mainWindow.btn_RightShift, "White") },
+                {Key.Enter, () => ResetHighlight(mainWindow.btn_Enter, "White") },
+                {Key.LWin, () => ResetHighlight(mainWindow.btn_LeftWin, "White") },
+                {Key.RWin, () => ResetHighlight(mainWindow.btn_RightWin, "White") }
+            };
+        }
+        public void OnKeyDown(object sender, KeyEventArgs e)
+        {
+            if (keyDownActions.TryGetValue(e.Key, out var action))
+                action();
+        }
+        public void OnKeyUp(object sender, KeyEventArgs e)
+        {
+            if (keyUpActions.TryGetValue(e.Key, out var action))
+                action();
+        }
+        private void HighlightButton(Button button, string content)
+        {
+            button.Background = new SolidColorBrush(Colors.DarkGray);
+            button.Content = content;
+        }
+        private void ResetHighlight(Button button, string hexColor)
+        {
+            var Converter = new BrushConverter();
+            button.Background = (Brush)Converter.ConvertFromString(hexColor);
+        }
+    }
+}
---
+++ InteractiveKeyboardTrainer/InteractiveKeyboardTrainer/MainWindow.xaml.cs
@@ -0,0 +1,282 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Threading;
+
+namespace InteractiveKeyboardTrainer
+{
+    public partial class MainWindow : Window
+    {
+        private DispatcherTimer sessionTimer; // таймер для запуска тренажера
+        private int elapsedTime = 0; // затраченное время
+        private int enteredChar = 0; // счетчик только для введенных символов
+        private string generatedText; // строка для генерации символов
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += KeyHandler_Load; // подключение словаря для неинтерактивных клавиш для подгрузки в главное окно
+            InitializeSession();
+        }
+        private void KeyHandler_Load(object sender, RoutedEventArgs e)
+        {
+            KeyboardHandler keyboardHandler = new KeyboardHandler(this);
+            KeyDown += keyboardHandler.OnKeyDown;
+            KeyUp += keyboardHandler.OnKeyUp;
+        }
+        private void Preview_OnDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Tab || e.Key == Key.Enter)
+            {
+                // Если нажали Tab или Enter, перемещаем фокус в InputTxt
+                InputTxt.Focus();
+                e.Handled = true;
+            }
+            else if (InputTxt.IsFocused)
+            {
+                InputTxt.KeyDown += InputTxt_KeyDown;
+                InputTxt.TextChanged += InputBox_Changed;
+            }
+        }
+        private void InitializeSession()
+        {
+            SetButtonState(StartTrainer, true);
+            SetButtonState(StopTrainer, false);
+        }
+        private void InputTxt_KeyDown(object sender, KeyEventArgs e)
+        {
+            bool ShiftPressed = (Keyboard.Modifiers & ModifierKeys.Shift) == ModifierKeys.Shift;
+            char typedChar = KeyToChar(e.Key, ShiftPressed);
+
+            if (typedChar != '\0')
+            {
+                InputTxt.AppendText(typedChar.ToString());
+                string userText = new TextRange(InputTxt.Document.ContentStart, InputTxt.Document.ContentEnd).Text;
+                int currentErrors = CalculateErrors(generatedText, userText, out int missedChars);
+                error_amount.Text = $"{currentErrors}";
+                enteredChar = userText.Length;
+                HighlightChar(generatedText, userText);
+                InputTxt.CaretPosition = InputTxt.Document.ContentEnd;
+                e.Handled = true;
+            }
+        }
+        private void InputBox_Changed(object sender, TextChangedEventArgs e)
+        {
+            string refText = new TextRange(TxtTrainer.Document.ContentStart, TxtTrainer.Document.ContentEnd).Text.Replace("\n", Environment.NewLine);
+            string typedText = new TextRange(InputTxt.Document.ContentStart, InputTxt.Document.ContentEnd).Text.Replace("\n", Environment.NewLine);
+
+            HighlightChar(refText, typedText);
+        }
+        private void DifficultyValue_Changed(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            int complexitylvl = (int)e.NewValue;
+            diff_text.Text = complexitylvl.ToString();
+
+            generatedText = GenerateRndString(complexitylvl, 25); // Меняется только уровень сложности, кол-во символов - по умолчанию
+            InitializeTrainerText(generatedText);
+        }
+        private void SymbolValue_Changed(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            int symb_overhaul = (int)e.NewValue;
+            symb_text.Text = symb_overhaul.ToString();
+
+            generatedText = GenerateRndString(1, symb_overhaul); // Меняется только кол-во символов, уровень сложности - по умолчанию
+            InitializeTrainerText(generatedText);
+        }
+        private void EventStarted_Click(object sender, RoutedEventArgs e)
+        {
+            SetButtonState(StartTrainer, false);
+            SetButtonState(StopTrainer, true);
+
+            generatedText = GenerateRndString((int)LvL_Setter.Value, 50);
+            InitializeTrainerText(generatedText);
+            StartSessionTimer();
+        }
+        private void EventStopped_Click(object sender, RoutedEventArgs e)
+        {
+            sessionTimer?.Stop();
+
+            string userText = new TextRange(InputTxt.Document.ContentStart, InputTxt.Document.ContentEnd).Text;
+            ShowSessionResults(generatedText, userText, elapsedTime);
+
+            SetButtonState(StartTrainer, true);
+            SetButtonState(StopTrainer, false);
+        }
+        private void Sensivity_Click(object sender, RoutedEventArgs e)
+        {
+            if (CaseSensivity.IsChecked == true)
+                InputTxt.KeyUp += EnforceUpperCase;
+            else if (CaseSensivity.IsChecked == false)
+                InputTxt.KeyUp -= EnforceUpperCase;
+        }
+        private string GenerateRndString(int complexitylvl, int length)
+        {
+            string lowerCase = "abcdefghijklmnopqrstuvwxyz"; // буквы нижнего регистра
+            string upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; // буквы верхнего регистра
+            string numbers = "0123456789"; // цифры
+            string specialChars = "!@#$%^&*()-_=+[{]};:'\",<.>/?"; // спец. символы
+            string ordinarySpace = " ";
+            string Spaces = " \t\n"; // символы табуляции
+
+            string availableChars = lowerCase + ordinarySpace; // Пусть это будет базовая сложность
+            if (complexitylvl >= 2) availableChars += upperCase + numbers + ordinarySpace; // для средней
+            if (complexitylvl >= 3) availableChars += specialChars + Spaces; // для сложной
+
+            StringBuilder result = new StringBuilder();
+            Random rnd = new Random();
+            for (int i=0; i < length; i++)
+            {
+                int index = rnd.Next(availableChars.Length);
+                result.Append(availableChars[index]);
+            }
+            return result.ToString();
+        }
+        private void InitializeTrainerText(string text)
+        {
+            if (TxtTrainer == null || string.IsNullOrEmpty(text)) return;
+
+            TxtTrainer.Document.Blocks.Clear(); // Очистка тек. текста
+
+            Paragraph paragraph = new Paragraph();
+            foreach (char c in generatedText)
+            {
+                // Создаём новый Run для каждого символа и устанавливаем черный цвет по умолчанию
+                Run run = new Run(c.ToString())
+                {
+                    Foreground = Brushes.Black
+                };
+                paragraph.Inlines.Add(run);
+            }
+
+            TxtTrainer.Document.Blocks.Add(paragraph);
+        }
+        private char KeyToChar(Key key, bool ShiftPressed)
+        {
+            string lowerCase = "abcdefghijklmnopqrstuvwxyz"; // нижние регистры
+            string upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; // верхние регистры
+            string numbers = "0123456789"; // цифры
+            string ShiftedChars = "!@#$%^&*()_+{}:|\"<>?"; // Спец. символы верхнего регистра
+            string UnshiftedChars = "1234567890-=[];',./"; // Спец. символы нижнего регистра
+
+            // Клавиши латинского алфавита (от A до Z)
+            if (key >= Key.A && key <= Key.Z)
+                return ShiftPressed ? upperCase[(int)key - (int)Key.A] : lowerCase[(int)key - (int)Key.A];
+            // Клавиши цифр (от 0 до 9)
+            if (key >= Key.D0 && key <= Key.D9)
+                return ShiftPressed ? ShiftedChars[(int)key - (int)Key.D0] : numbers[(int)key - (int)Key.D0];
+            // Спец. Oem клавиши и прочее
+            if (key >= Key.Oem1 && key <= Key.OemTilde)
+                return ShiftPressed ? ShiftedChars[(int)key - (int)Key.Oem1] : UnshiftedChars[(int)key - (int)Key.Oem1];
+            if (key == Key.Space) return ' ';
+            if (key == Key.Enter) return '\n';
+            if (key == Key.Tab) return '\t';
+
+            return '\0';
+        }
+        private void HighlightChar(string targetedText, string typedText)
+        {
+            TxtTrainer.Document.Blocks.Clear();
+            var paragraph = new Paragraph();
+
+            for (int i = 0; i < targetedText.Length; i++)
+            {
+                char currentRefChar = targetedText[i];
+                Run charRun = new Run(currentRefChar.ToString());
+
+                if (i < typedText.Length)
+                {
+                    char currentTypedChar = typedText[i];
+                    charRun.Foreground = currentTypedChar == currentRefChar ? Brushes.Green : Brushes.Red;
+                }
+                else if (i == typedText.Length)
+                {
+                    charRun.Foreground = Brushes.Gray;
+                }
+                else
+                {
+                    charRun.Foreground = Brushes.Black;
+                }
+                paragraph.Inlines.Add(charRun);
+            }
+            TxtTrainer.Document.Blocks.Add(paragraph);
+        }
+        private void StartSessionTimer()
+        {
+            sessionTimer = new DispatcherTimer() { Interval = TimeSpan.FromSeconds(1) };
+            sessionTimer.Tick += (s, e) => { elapsedTime++; speed_amount.Text = $"{elapsedTime} сек"; symbol_amount.Text = $"{enteredChar}"; };
+            elapsedTime = 0;
+            enteredChar = 0;
+            sessionTimer.Start();
+        }
+        private int CalculateErrors(string refText, string userText, out int missedChars)
+        {
+            int errorCount = 0;
+            missedChars = 0;
+            int length = Math.Min(refText.Length, userText.Length);
+
+            for (int i = 0; i < length; i++)
+            {
+                if (refText[i] != userText[i])
+                    errorCount++;
+            }
+            if (userText.Length < refText.Length)
+                missedChars = refText.Length - userText.Length;
+            errorCount += Math.Abs(refText.Length - userText.Length);
+
+            return errorCount;
+        }
+        private double CalculateSpeed(string userText, int elapsedTime)
+        {
+            return elapsedTime > 0 ? userText.Length / (elapsedTime / 60.0) : 0;
+        }
+        private void SetButtonState(UIElement button, bool isEnabled)
+        {
+            button.IsEnabled = isEnabled;
+            button.Opacity = isEnabled ? 1.0 : 0.5;
+        }
+        private void ResetTrainer()
+        {
+            TxtTrainer.Document.Blocks.Clear(); // очистка текстового поля для генерированных строк
+            InputTxt.Document.Blocks.Clear(); // очистка текстового поля для ввода
+            generatedText = ""; // опустошаем весь текст
+            elapsedTime = 0;
+            enteredChar = 0;
+            sessionTimer = null; // сброс всего таймера
+            error_amount.Text = "0"; // сброс в нуль
+            speed_amount.Text = "0 сек"; // сброс в нуль секунд
+        }
+        private void ShowSessionResults(string refText, string userText, int elapsedTime)
+        {
+            int errorCount = CalculateErrors(refText, userText, out int missedChars);
+            if (string.IsNullOrEmpty(userText))
+            {
+                errorCount = 0;
+                missedChars = refText.Length;
+            }
+            double speed = CalculateSpeed(userText, elapsedTime);
+            MessageBox.Show($"Ошибок: {errorCount} \nПропущенных символов: {missedChars} \nСкорость: {speed:F1} символов/мин", "Результат Тренировки", MessageBoxButton.OK);
+            ResetTrainer();
+        }
+        private void EnforceUpperCase(object sender, KeyEventArgs e)
+        {
+            if (e.Key != Key.Back && e.Key != Key.Enter)
+            {
+                var TextPointer = InputTxt.CaretPosition.GetPositionAtOffset(-1, LogicalDirection.Forward);
+                if (TextPointer != null)
+                {
+                    var lastChar = TextPointer.GetTextInRun(LogicalDirection.Forward);
+                    if (!string.IsNullOrEmpty(lastChar) && char.IsLower(lastChar[0]))
+                    {
+                        TextPointer.DeleteTextInRun(-1);
+                        TextPointer.InsertTextInRun(char.ToUpper(lastChar[0]).ToString());
+                        InputTxt.CaretPosition = TextPointer.GetPositionAtOffset(1, LogicalDirection.Forward);
+                    }
+                }
+            }
+        }
+    }
+}
